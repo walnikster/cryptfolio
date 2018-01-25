25ee70edf803125543f71b68e57971c67979fdb2
niki <niki@theliferocks.com>
2018-01-25 18:33:31 +0100

    bootstrap 4.0

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,35 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-     <a class="navbar-brand" href="#">Navbar</a>
-     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-     aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-icon"></span>
-     </button>
-     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-       <ul class="navbar-nav mr-auto">
-         <li class="nav-item active"><router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/">overview</router-link></li>
-         <li class="nav-item"><router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/details">details</router-link></li>
-         <li class="nav-item"><router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/add">add</router-link></li>
-         <li class="nav-item"><router-link data-toggle="collapse" data-target=".navbar-collapse" class="nav-link" to="/edit">edit</router-link></li>
-       </ul>
-     </div>
-   </nav>
+<div>    
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="/">Cryptfolio</a>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active"><router-link data-target=".navbar-collapse" class="nav-link" to="/">overview</router-link></li>
+         <li class="nav-item"><router-link data-target=".navbar-collapse" class="nav-link" to="/details">details</router-link></li>
+         <li class="nav-item"><router-link data-target=".navbar-collapse" class="nav-link" to="/add">add</router-link></li>
+         <li class="nav-item"><router-link data-target=".navbar-collapse" class="nav-link" to="/edit">edit</router-link></li>
+    </ul>
+  </div>
+</nav>
     <main role="main" class="container">
-      <div class="starter-template">
         <router-view/>
-      </div>
     </main>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app'
-  }
+export default {
+  name: "app"
+};
 </script>
 
-<style scoped>
-.starter-template {
-    padding: 3rem 1.5rem;
-    text-align: center;
-  }
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 </style>
